9cd88ce49bcc06cb6e82c00a9d3d0efff58c3c74
Youssef Kandil <youssefkandil@aucegypt.edu>
2024-07-18 17:40:09 +0300

    bench format

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ def main():
     # args = parser.parse_args()
     # input_file = args.input_file
 
-    input_file = "/Users/youssef/Documents/Work/GSOC/PODEM-ATPG/test/c17.txt"
+    input_file = "/Users/youssef/Documents/Work/GSOC/PODEM-ATPG/test/c17.bench"
     fault_file = "/Users/youssef/Documents/Work/GSOC/PODEM-ATPG/test/c17.fault"
     circuit = Circuit()
     circuit.parse_circuit_file(input_file)
